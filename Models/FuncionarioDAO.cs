658229c7fcfc777fa9c442de0ffac32d343ee947
Joao <joao.freire@estudante.ifro.edu.br>
2022-11-23 08:49:53 -0300

    Update FuncionarioDAO.cs

--- Models/FuncionarioDAO.cs
+++ Models/FuncionarioDAO.cs
@@ -3,7 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
 using sislocacao.bancodados;
+using sislocacao.Helpers;
 
 namespace sislocacao.Models
 {
@@ -42,5 +44,43 @@ namespace sislocacao.Models
                 throw ex;
             }
         }
+
+        public List<Funcionario> List()
+        {
+            try
+            {
+                var comando = _conn.Query();
+
+                var lista = new List<Funcionario>();
+
+                comando.CommandText = "select * from Funcionario;";
+
+                MySqlDataReader reader = comando.ExecuteReader();
+
+                while (reader.Read())
+                {
+                    var Funcionario = new Funcionario();
+                    Funcionario.Id = reader.GetInt32("id_func");
+                    Funcionario.Nome = DAOHelper.GetString(reader, "nome_func");
+                    Funcionario.CPF = DAOHelper.GetString(reader, "cpf_func");
+                    Funcionario.RG = DAOHelper.GetString(reader, "rg_func");
+                    Funcionario.DataNasc = DAOHelper.GetDateTime(reader, "data_nascimento_func");
+                    Funcionario.Salario = DAOHelper.GetDouble(reader, "salario_func");
+                    Funcionario.Endereco = DAOHelper.GetString(reader, "endereco_func");
+                    Funcionario.Celular = DAOHelper.GetString(reader, "celular_func");
+                    Funcionario.Funcao = DAOHelper.GetString(reader, "funcao_func");
+
+                    lista.Add(Funcionario);
+                }
+                reader.Close();
+                return lista;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
     }
+
+    
 }
