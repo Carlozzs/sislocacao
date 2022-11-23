0ee54bae9147b1f6790c0fdc5e211ea413199733
Joao <joao.freire@estudante.ifro.edu.br>
2022-11-23 08:51:57 -0300

    Update CarroDAO.cs

--- Models/CarroDAO.cs
+++ Models/CarroDAO.cs
@@ -3,7 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
 using sislocacao.bancodados;
+using sislocacao.Helpers;
 
 namespace sislocacao.Models
 {
@@ -40,6 +42,40 @@ namespace sislocacao.Models
             catch (Exception ex)
             {
 
+                throw ex;
+            }
+        }
+
+        public List<Carro> List()
+        {
+            try
+            {
+                var comando = _conn.Query();
+
+                var lista = new List<Carro>();
+
+                comando.CommandText = "select * from Carro;";
+
+                MySqlDataReader reader = comando.ExecuteReader();
+
+                while (reader.Read())
+                {
+                    var Carro = new Carro();
+                    Carro.Id = reader.GetInt32("id_car");
+                    Carro.Modelo = DAOHelper.GetString(reader, "modelo_car");
+                    Carro.Cor = DAOHelper.GetString(reader, "cor_car");
+                    Carro.Porta = DAOHelper.GetString(reader, "portas_car");
+                    Carro.Placa = DAOHelper.GetString(reader, "placa_car");
+                    Carro.Marca = DAOHelper.GetString(reader, "marca_car");
+                    Carro.Status = DAOHelper.GetString(reader, "status_car");
+
+                    lista.Add(Carro);
+                }
+                reader.Close();
+                return lista;
+            }
+            catch (Exception ex)
+            {
                 throw ex;
             }
         }
